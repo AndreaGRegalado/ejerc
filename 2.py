937914778ff4df17ad819269774770ea9c83299b
Andrea Garcia <andrea.garcia@ieo.csic.es>
2024-02-26 15:17:29 +0100

    Final día 260224

--- 2.py
+++ 2.py
@@ -18,4 +18,29 @@ x_1, y_1 = np.meshgrid(x, y)
 plt.pcolormesh(x_1)
 plt.pcolormesh(y_1)
 
+##1.3 From your cartesian coordinates, create polar coordinates "r" and "phi"
+##You will need to use numpy’s arctan2 function. Read its documentation.
 
+def cart2pol (x,y):
+    r = np.sqrt(x**2 + y**2)
+    phi = np.arctan2(y,x)
+    return r, phi
+
+r, phi = cart2pol(x, y)
+
+##1.4. Visualize "r" and "phi" on the 2D x/y plane using pcolormesh
+
+##1.5 Caclulate the quanity (fórmula)
+
+##1.6 Plot the mean of f with respect to the x axis as a function of y
+
+##1.7 Plot the mean of f with respect to the y axis as a function of x
+
+##1.8 1.8 Plot the mean of f with respect to   as a function of  
+
+###2. Analyze ARGO Data
+
+import pooch
+url = "https://www.ldeo.columbia.edu/~rpa/float_data_4901412.zip"
+files = pooch.retrieve(url, processor=pooch.Unzip(), known_hash="2a703c720302c682f1662181d329c9f22f9f10e1539dc2d6082160a469165009")
+files
