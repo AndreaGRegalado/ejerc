56115faf8f0a7d06a7a6b2411917123a8c7fa41d
Andrea Garcia <andrea.garcia@ieo.csic.es>
2024-03-01 12:14:15 +0100

    Empezar de nuevo

--- 2.2.1.py
+++ 2.2.1.py
@@ -21,30 +21,35 @@ lat = ds.Y.values
 ######################################
 
 from matplotlib import pyplot as plt
+import numpy as np
 
 
 #GridSpec(nrows, ncols, figure=None, left=None, bottom=None, right=None,
 #top=None, wspace=None, hspace=None, width_ratios=None, height_ratios=None)
-fig, axes = plt.subplots(figsize=(12, 10), nrows = 1, ncols = 2, gridspec_kw={'width_ratios': [5, 2],
-                                            "wspace":0})
+fig, axes = plt.subplots(figsize=(15, 6.5), nrows = 1, ncols = 2, gridspec_kw={'width_ratios': [10, 3],
+                                            "wspace":0.025})
 #fig, axes = plt.subplots(figsize=(12, 10),nrows=3) 
 #Mapa
 mapa = axes[0].contourf(lon, lat, temp, 
-                        cmap='magma', 
+                        cmap='magma',
+                        levels=20,
                         extend='both')
-axes[0].set_title("Current Global Temperature",
-                  fontsize=8)
+axes[0].set_title("Current Global Temperature"
+                  )
 axes[0].set_xlabel(xlabel="Longitude",
-                   fontsize=8)
+                   fontsize=13)
 axes[0].set_ylabel(ylabel="Latitude",
-                   fontsize=8)
+                   fontsize=13)
 axes[0].grid(which='major',
              linestyle='--',
              color="black",
              linewidth=0.5)
 
-fig.tight_layout()
+#fig.tight_layout()
 plt.colorbar(mapa,
-             label="ªC")
+             label="ªC",
+             shrink=0.5)
 
-#axes[1].plot(lon, temp)
+media_por_latitud = np.mean(temp, axis=1)
+
+axes[1].plot(media_por_latitud,lat)
