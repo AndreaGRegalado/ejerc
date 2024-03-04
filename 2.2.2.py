ed0888ad770153de6d078d582b83feb8264aab54
Andrea Garcia <andrea.garcia@ieo.csic.es>
2024-03-04 10:37:48 +0100

    Fin ejercicio 2.2.2, a falta de corregir escala logarítmica

--- 2.2.2.py
+++ 2.2.2.py
@@ -8,12 +8,13 @@ Ejer3 - Scatter Plots
 from matplotlib import pyplot as plt
 import numpy as np
 import pooch
+import matplotlib.colors
+
 
 fname = pooch.retrieve(
     "https://rabernat.github.io/research_computing/signif.txt.tsv.zip",
     known_hash='22b9f7045bf90fb99e14b95b24c81da3c52a0b4c79acf95d72fbe3a257001dbb',
-    processor=pooch.Unzip()
-)[0]
+    processor=pooch.Unzip())[0]
 
 earthquakes = np.genfromtxt(fname, delimiter='\t')
 depth = earthquakes[:, 8]
@@ -21,7 +22,21 @@ magnitude = earthquakes[:, 9]
 latitude = earthquakes[:, 20]
 longitude = earthquakes[:, 21]
 ####################
-fig, ax = plt.subplots()
+#Monto la figura, con esas medidas
+fig, ax = plt.subplots(figsize=(15, 8))
+#Tamaño de los circulos atendiendo a la magnitud
+sizes=magnitude**4/100
+#Monto mapa, con tamaño circulos y colores de profundidad, la escala logaritmica no me salió
+mapa = ax.scatter(longitude, latitude, s=sizes, c=np.log10(depth),
+                  norm=matplotlib.colors.LogNorm())
+#Barra de escala
+fig.colorbar(mapa,
+             label="Depth[m]")
+ax.set_title("Location of Significant Earthquakes with size indicating relative magnitude")
+ax.set_xlabel(xlabel="Longitude(°)")
+ax.set_ylabel(ylabel="Latitude (°)")
+ax.grid(which='major',
+             color="grey",
+             linewidth=0.5)
 
-mapa = ax.scatter(longitude, latitude, s=magnitude, c=np.log10(depth))
-fig.colorbar(mapa)
+
